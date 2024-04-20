cfb0b3305d56d52a94f587a3e9211de29772542b
Nikolai2038 <nikolai2038@yandex.ru>
2024-04-20 21:19:43 +0300

    Create MFC project

---
+++ SpaceBattleSolution/SpaceBattle/SpaceBattle.h
@@ -0,0 +1,32 @@
+
+// SpaceBattle.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CSpaceBattleApp:
+// See SpaceBattle.cpp for the implementation of this class
+//
+
+class CSpaceBattleApp : public CWinApp
+{
+public:
+	CSpaceBattleApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSpaceBattleApp theApp;
