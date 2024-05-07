a18494216ce4ef6e9868f778ba80accd7b4125a3
Nikolai2038 <nikolai2038@yandex.ru>
2024-05-07 17:05:34 +0300

    Add icons for game dialog

--- SpaceBattleSolution/SpaceBattle/Resource.h
+++ SpaceBattleSolution/SpaceBattle/Resource.h
@@ -8,10 +8,14 @@
 #define IDD_DIALOG_RULES                129
 #define IDD_DIALOG_GAME                 131
 #define IDD_DIALOG_RECORDS              133
-#define IDB__BULLET                     135
 #define IDB_BULLET                      135
 #define IDB_ENEMY                       136
 #define IDB_PLAYER                      137
+#define IDI_POINT                       141
+#define IDI_ENEMIES                     142
+#define IDI_TIME                        143
+#define IDI_ICON3                       144
+#define IDI_HEALTH                      144
 #define IDC_BUTTON_PLAY                 1000
 #define IDC_BUTTON_RULES                1001
 #define IDC_BUTTON_PAUSE_GAME           1001
@@ -26,14 +30,18 @@
 #define IDC_TEXT_TIME_PLAYING           1011
 #define IDC_GAME_SCREEN                 1012
 #define IDC_TEXT_RULES_1                1013
+#define IDC_POINT                       1014
+#define IDC_POINT2                      1015
+#define IDC_POINT3                      1016
+#define IDC_POINT4                      1017
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_RESOURCE_VALUE        145
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_CONTROL_VALUE         1015
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
